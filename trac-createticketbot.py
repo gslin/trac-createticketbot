827d64920259f3f8bd422eab1d8dac56152f1c0a
Gea-Suan Lin <gslin@gslin.org>
2018-02-20 00:22:51 +0800

    Read config.

--- trac-createticketbot.py
+++ trac-createticketbot.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 
+import configparser
+import os
+
 class TracCreateTicketBot(object):
     def __init__(self):
         pass
 
     def start(self):
-        pass
+        home = os.environ['HOME']
+        f_conf = '{}/.config/trac-createticketbot/config.ini'.format(home)
+
+        c = configparser.ConfigParser()
+        c.read(f_conf)
 
 if '__main__' == __name__:
     t = TracCreateTicketBot()
