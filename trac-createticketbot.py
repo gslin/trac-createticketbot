de0c8fc6505e2ae28b0d6902ad4a7b69c32c0ed9
Gea-Suan Lin <gslin@gslin.org>
2018-05-14 09:17:27 +0800

    Using localtime.

--- trac-createticketbot.py
+++ trac-createticketbot.py
@@ -47,7 +47,7 @@ class TracCreateTicketBot(object):
             elif '--priority' == opt:
                 a['priority'] = arg
             elif '--title' == opt:
-                title = arg
+                title = time.strftime(arg, time.localtime())
 
         id = self.s.ticket.create(title, description, a)
         print('* ticket_id = {}'.format(id))
